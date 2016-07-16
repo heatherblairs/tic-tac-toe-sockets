01d64bb756802b773a0a8af38ddb9c4c9126296b
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2016-07-16 16:40:43 -0600

    add sockets functionality

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get('/', function(request, response, next) {
+  response.render('index');
 });
 
 module.exports = router;
--- socket.js
+++ socket.js
@@ -1,3 +1,32 @@
+var Socket = require('socket.io');
+var TicTacToe = require('./tic-tac-toe');
+var game = new TicTacToe();
+var gameDimensions;
+
 module.exports = function (server) {
+
+  var io = Socket(server);
+
+  io.on('connection', function(socket) {
+    io.emit('new-connection', game);
+
+    socket.on('new-game', function(dimensions) {
+      gameDimensions = dimensions;
+      game.createGame(dimensions);
+      io.emit('load', game);
+    });
+
+    socket.on('move', function(location) {
+      game.move(location);
+      io.emit('load', game);
+    });
+
+    socket.on('reset', function() {
+      game.reset();
+      game.createGame(gameDimensions);
+      io.emit('load', game);
+    });
+
+  });
 
 };
